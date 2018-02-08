35a640327c199c7ea22670944105b62abb3d71a2
luis briseño <guicho72@hotmail.com>
2018-02-08 16:22:50 -0600

    CRatio-maqueta

---
+++ NetCorePSAV/Controllers/CRatioController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NetCorePSAV.Controllers
+{
+    public class CRatioController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult CaptureRatio()
+        {
+            return View();
+        }
+        public IActionResult NewCR()
+        {
+            return View();
+        }
+    }
+}
